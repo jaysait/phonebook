e360ba1cecb2dbcc866eca2b24b9ca892024ffee
Jason Fleming <jason.fleming.se@gmail.com>
2021-10-29 14:55:19 -0600

    exercises 2.6 - 2.8 fullstackopen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Jay Rard', number: '403-555-0000' }]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const handleChangeName = (event) => {
+    setNewName(event.target.value);
+  };
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName, number: newNumber };
+    const alreadyExists = persons.filter((p) => p.name === newName).length;
+
+    if (alreadyExists > 0) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons([...persons, newPerson]);
+      setNewName('');
+      setNewNumber('');
+    }
+  };
   return (
-    <div className='App'>
-      <header className='App-header'></header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input id='new-person-name' value={newName} onChange={handleChangeName} />
+        </div>
+        <div>
+          number: <input id='new-person-number' value={newNumber} onChange={handleChangeNumber} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
